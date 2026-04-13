5de89c12131d4fc6820cff459fe4c6da638f63c7
kensukeshouni <6532835+shouni@users.noreply.github.com>
2026-04-13 09:12:03 +0900

    Refactor: remove `go-http-kit` dependency, replace with `ports.Downloader` interface, and update references in `GeminiImageCore` and `README.md`.

--- ports/io.go
+++ ports/io.go
@@ -15,3 +15,9 @@ type ContentWriter interface {
 	// Write は、指定された path または URI へデータを書き込みます。
 	Write(ctx context.Context, path string, contentReader io.Reader, contentType string) error
 }
+
+// Downloader は URL からデータをダウンロードし、提供された関数を使用してデータ ストリームを処理するためのインターフェイスを定義します。
+type Downloader interface {
+	FetchStream(ctx context.Context, url string, fn func(io.Reader) error) error
+	GetStream(ctx context.Context, url string) (io.ReadCloser, error)
+}
